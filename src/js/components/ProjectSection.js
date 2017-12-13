0ab7aa0e0f71c3378e9cb5d66c1e247654a7d350
Kshitij Karthick <kshitij.karthick@gmail.com>
2017-12-13 09:06:19 +0530

    ProjectSection: Setup with demo projects

--- src/js/components/ProjectSection.js
+++ src/js/components/ProjectSection.js
@@ -1,9 +1,16 @@
-import Project from './Project';
 import React, { Component } from 'react';
+
+import Anchor from 'grommet/components/Anchor';
+import Article from 'grommet/components/Article';
+import Box from 'grommet/components/Box';
 import Header from 'grommet/components/Header';
-import Heading from 'grommet/components/Heading';
+import Menu from 'grommet/components/Menu';
+import Project from './Project';
+import R from 'ramda';
 import Section from 'grommet/components/Section';
-import Scorecard from 'grommet/components/icons/base/Scorecard';
+import Sidebar from 'grommet/components/Sidebar';
+import Split from 'grommet/components/Split';
+import Title from 'grommet/components/Title';
 
 export default class ProjectSection extends Component {
   constructor(props) {
@@ -17,7 +24,7 @@ export default class ProjectSection extends Component {
             'Provide capability to find semantically related words across languages in a agnostic way.',
             'Given a word in language 1, you can find semantically related words in language 2.',
             'The model can be used to obtain a semantic distance measure for words across languages.'
-          ],            
+          ],
           textAlign: 'left',
           category: 'Machine Learning',
           languageDistribution: [
@@ -40,7 +47,7 @@ export default class ProjectSection extends Component {
             ' Visualize and interact with the data on the Map of Bengaluru'
           ],
           textAlign: 'left',
-          category: 'Machine Learning',
+          category: 'Data Mining',
           languageDistribution: [
             {"label": "Python", "value": 40, "colorIndex": "graph-1"},
             {"label": "Javascript", "value": 30, "colorIndex": "graph-2"},
@@ -57,10 +64,46 @@ export default class ProjectSection extends Component {
       ]
     };
   }
-  getProjectHTML() {
-    return this.state.projectData.map((project, index) => {
+  getSortedCategories() {
+    var sortCategories = R.sortWith([R.ascend(R.prop('category'))]);
+    var sortedCategories = R.uniq(
+      R.pluck('category')(sortCategories(this.state.projectData))
+    );
+    return sortedCategories;
+  }
+  getCategorySidebar() {
+    var categories = this.getSortedCategories();
+    var getAnchorForCategories = categories.map((category, index) => {
+      return (
+        <Anchor key={index}>
+            {category}
+        </Anchor>
+      );
+    });
+    return (<Sidebar
+      full={true}>
+      <Header pad='medium'
+        justify='between'>
+        <Title>
+          Categories
+        </Title>
+      </Header>
+      <Box flex='grow'
+        justify='start'>
+        <Menu primary={true}>
+          {getAnchorForCategories}
+        </Menu>
+      </Box>
+    </Sidebar>);
+  }
+  getProjects() {
+    var groupByCategories = R.groupBy(R.prop('category'));
+    var sortedCategories = this.getSortedCategories();
+    var categoryBasedProjects = groupByCategories(this.state.projectData);
+    var getProject = ((project, index) => {
       return (
         <Project
+          id={project.category}
           key={index}
           title={project.title}
           description={project.description}
@@ -73,18 +116,32 @@ export default class ProjectSection extends Component {
         />
       );
     });
+    var getCategoryBasedProjectList = ((category, index) => {
+      return (
+        <Section key={category}>
+          {categoryBasedProjects[category].map(getProject)}
+        </Section>
+      );
+    });
+    return sortedCategories.map(getCategoryBasedProjectList);
   }
-  render() {    
+  render() {
     return (
-      <Section id="Projects" pad={{between: 'medium', vertical: 'small'}}>
-        <Header justify="between">
-          <Heading tag="h2">
-            < Scorecard /> Projects
-          </Heading>
-        </Header>
-        {this.getProjectHTML()}
-      </Section>
+      <Article pad={{between: 'small'}}>
+        <Split priority='left' fixed={false} flex='left'>
+          <Box
+            justify='center'
+            size='large'
+            align='center'>
+              {this.getProjects()}
+          </Box>
+          <Box
+            justify='center'
+            align='center'>
+              <Box>{this.getCategorySidebar()}</Box>
+          </Box>
+        </Split>
+      </Article>
     );
   }
 }
-
