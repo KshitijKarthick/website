59c97d17eaa15a1af2678f01ce53841069a59d70
Kshitij Karthick <kshitij.karthick@gmail.com>
2017-12-13 15:25:20 +0530

    ProjectSection: Retrieve project list from external source.

--- src/js/components/ProjectSection.js
+++ src/js/components/ProjectSection.js
@@ -11,57 +11,14 @@ import Section from 'grommet/components/Section';
 import Sidebar from 'grommet/components/Sidebar';
 import Split from 'grommet/components/Split';
 import Title from 'grommet/components/Title';
+import projects from '../data/projects';
 
 export default class ProjectSection extends Component {
   constructor(props) {
     super(props);
     // Operations usually carried out in componentWillMount go here
     this.state = {
-      projectData: [
-        {
-          title: 'Measuring Semantic Relatedness Across Languages',
-          description: [
-            'Provide capability to find semantically related words across languages in a agnostic way.',
-            'Given a word in language 1, you can find semantically related words in language 2.',
-            'The model can be used to obtain a semantic distance measure for words across languages.'
-          ],
-          textAlign: 'left',
-          category: 'Machine Learning',
-          languageDistribution: [
-            {"label": "Python", "value": 70, "colorIndex": "graph-1"},
-            {"label": "Javascript", "value": 10, "colorIndex": "graph-2"},
-            {"label": "CSS", "value": 5, "colorIndex": "graph-3"},
-            {"label": "HTML", "value": 15, "colorIndex": "graph-4"}
-          ],
-          commits: {
-            'mine': 294,
-            'total': 420
-          },
-          github: 'http://github.com/kshitijkarthick/tvecs/',
-          demo: 'http://kshitijkarthick.github.io/tvecs/'
-        },
-        {
-          title: 'Urban Data Visualization',
-          description: [
-            'Provide capability to obtain insights on the Wards of Bengaluru based on factors like number of parks, toilets, lakes, population density.',
-            ' Visualize and interact with the data on the Map of Bengaluru'
-          ],
-          textAlign: 'left',
-          category: 'Data Mining',
-          languageDistribution: [
-            {"label": "Python", "value": 40, "colorIndex": "graph-1"},
-            {"label": "Javascript", "value": 30, "colorIndex": "graph-2"},
-            {"label": "CSS", "value": 5, "colorIndex": "graph-3"},
-            {"label": "HTML", "value": 25, "colorIndex": "graph-4"}
-          ],
-          commits: {
-            'mine': 100,
-            'total': 150
-          },
-          github: null,
-          demo: 'http://udvhack.herokuapp.com/'
-        }
-      ]
+      projectData: projects.projectList
     };
   }
   getSortedCategories() {
