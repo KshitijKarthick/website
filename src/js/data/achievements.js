27f797c4ad944b4d0b1e6862282775c8e7ac5f7b
Kshitij Karthick <kshitij.karthick@gmail.com>
2017-12-15 18:53:49 +0530

    Data: Add achievements js

---
+++ src/js/data/achievements.js
@@ -0,0 +1,10 @@
+exports.acheivementsList = [
+  "Judged two National Level Hackathons at Reva University 2017, 2016.",
+  "Completed the Digital Ocean Hacktoberfest Challenge 2017, 2016.",
+  "Inter College 24hr Hackathon (Team of 5), Reva ITM 2016. (1stplaceforUDV-Hack).",
+  "National Students Project Exhibition (Team of 4), AICTE CSE 2016. (1stplaceforT-VecsProject).",
+  "Computer Graphics (CG) Mini Project Exhibition, Reva ITM 2015. Awarded 3rd place.",
+  "Code-a-thon, Zest Arise 2015. (1stplaceDepartmentfest).",
+  "Code-Vita, Qualified within top200teamsinIndia, 2015._",
+  "Experimental Physics, IISc Pravega 2014. Placed Runner-up in the Experimental Physics Contest."
+];
