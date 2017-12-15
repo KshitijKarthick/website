2286a6ccfa14d1491a24e542f057df25557f89b8
Kshitij Karthick <kshitij.karthick@gmail.com>
2017-12-15 10:38:17 +0530

    SkillsData: Add skills data

--- src/js/data/interests.js
+++ src/js/data/interests.js
@@ -1,10 +1,38 @@
 exports.interestDistributionData = [
-    {"label": "Machine Learning / Deep Learning", "value": 30},
-    {"label": "Data Science & Analytics", "value": 20, "colorIndex": "accent-2"},
-    {"label": "Web Development", "value": 20, "colorIndex": "unset"},
-    {"label": "Functional Programming", "value": 15},
-    {"label": "Visualizations", "value": 15},
-    {"label": "Reading Novels", "value": 10},
-    {"label": "Music", "value": 20, "colorIndex": "accent-1"},
-    {"label": "FOSS", "value": 15, "colorIndex": "accent-4"}     
+    {"label": "Machine Learning / Deep Learning", "value": 30, "labelValue": ""},
+    {"label": "Data Science & Analytics", "value": 20, "colorIndex": "accent-2", "labelValue": ""},
+    {"label": "Web Development", "value": 20, "colorIndex": "unset", "labelValue": ""},
+    {"label": "Functional Programming", "value": 15, "labelValue": ""},
+    {"label": "Visualizations", "value": 15, "labelValue": ""},
+    {"label": "Reading Novels", "value": 10, "labelValue": ""},
+    {"label": "Music", "value": 20, "colorIndex": "accent-1", "labelValue": ""},
+    {"label": "FOSS", "value": 15, "colorIndex": "accent-4", "labelValue": ""}     
+];
+
+exports.skillsDistributionData = [
+    {"label": "Python", "value": 30, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "Javascript", "value": 30, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "Scala", "value": 20, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "Java", "value": 20, "colorIndex": "graph-2", "labelValue": ""},
+    {"label": "C", "value": 10, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "C++", "value": 10, "colorIndex": "graph-3", "labelValue": ""},
+    {"label": "Ruby", "value": 20, "colorIndex": "graph-2", "labelValue": ""},
+    {"label": "HTML", "value": 20, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "CSS", "value": 10, "colorIndex": "graph-3", "labelValue": ""},
+    {"label": "Git", "value": 20, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "Pandas", "value": 30, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "Keras", "value": 20, "colorIndex": "graph-2", "labelValue": ""},
+    {"label": "Scikit-Learn", "value": 30, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "Seaborn", "value": 20, "colorIndex": "graph-2", "labelValue": ""},
+    {"label": "Matplotlib", "value": 10, "colorIndex": "graph-3", "labelValue": ""},
+    {"label": "Docker", "value": 30, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "MongoDB", "value": 30, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "Elk", "value": 20, "colorIndex": "graph-3", "labelValue": ""},
+    {"label": "React", "value": 20, "colorIndex": "graph-2", "labelValue": ""},
+    {"label": "Angular 1", "value": 20, "colorIndex": "graph-2", "labelValue": ""},
+    {"label": "CherryPy", "value": 30, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "Flask", "value": 30, "colorIndex": "graph-2", "labelValue": ""},
+    {"label": "Falcon", "value": 30, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "Bootstrap", "value": 20, "colorIndex": "graph-2", "labelValue": ""},
+    {"label": "Grommet", "value": 20, "colorIndex": "graph-3", "labelValue": ""}
 ];
