9c3ca2a79919d5d074e1d04e58a5118a317eb9b4
Kshitij Karthick <kshitij.karthick@gmail.com>
2020-10-18 16:23:48 +0200

    Add achievements for judge in hackathons

--- src/js/data/achievements.js
+++ src/js/data/achievements.js
@@ -1,10 +1,10 @@
 exports.acheivementsList = [
-  "Judged two National Level Hackathons at Reva University 2017, 2016.",
+  "Judged two National Level Hackathons at Reva University 2019, 2017, 2016.",
   "Completed the Digital Ocean Hacktoberfest Challenge 2017, 2016.",
-  "Inter College 24hr Hackathon (Team of 5), Reva ITM 2016. (1stplaceforUDV-Hack).",
-  "National Students Project Exhibition (Team of 4), AICTE CSE 2016. (1stplaceforT-VecsProject).",
+  "Inter College 24hr Hackathon (Team of 5), Reva ITM 2016. (1st for UDV-Hack).",
+  "National Students Project Exhibition (Team of 4), AICTE CSE 2016. (1st for T-Vecs Project).",
   "Computer Graphics (CG) Mini Project Exhibition, Reva ITM 2015. Awarded 3rd place.",
-  "Code-a-thon, Zest Arise 2015. (1stplaceDepartmentfest).",
-  "Code-Vita, Qualified within top200teamsinIndia, 2015._",
+  "Code-a-thon, Zest Arise 2015. (1st Department fest).",
+  "Code-Vita, Qualified within top 200 teams in India, 2015.",
   "Experimental Physics, IISc Pravega 2014. Placed Runner-up in the Experimental Physics Contest."
 ];
