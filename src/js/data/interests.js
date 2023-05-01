a977db6bbc3af11330e189aadac4c923e55d69cd
Kshitij Karthick <kshitij.karthick@gmail.com>
2023-05-01 09:24:09 +0530

    Update skills and colour index for them

--- src/js/data/interests.js
+++ src/js/data/interests.js
@@ -10,23 +10,28 @@ exports.interestDistributionData = [
 
 exports.skillsDistributionData = [
     {"label": "Python", "value": 40, "colorIndex": "accent-2", "labelValue": ""},
+    {"label": "Flask", "value": 15, "colorIndex": "accent-2", "labelValue": ""},
+    {"label": "FastAPI", "value": 15, "colorIndex": "accent-2", "labelValue": ""},
+
     {"label": "Pandas", "value": 15, "colorIndex": "graph-2", "labelValue": ""},
+    {"label": "Numpy", "value": 5, "colorIndex": "graph-2", "labelValue": ""},
     {"label": "Pytorch", "value": 15, "colorIndex": "graph-2", "labelValue": ""},
-    {"label": "Keras", "value": 10, "colorIndex": "graph-2", "labelValue": ""},
-    {"label": "Scikit-Learn", "value": 10, "colorIndex": "graph-2", "labelValue": ""},
-    {"label": "CherryPy", "value": 10, "colorIndex": "graph-3", "labelValue": ""},
-    {"label": "Flask", "value": 10, "colorIndex": "graph-3", "labelValue": ""},
+    {"label": "Keras", "value": 5, "colorIndex": "graph-2", "labelValue": ""},
+    {"label": "Scikit-Learn", "value": 7, "colorIndex": "graph-2", "labelValue": ""},
 
-    {"label": "Javascript", "value": 25, "colorIndex": "accent-2", "labelValue": ""},
-    {"label": "HTML", "value": 15, "colorIndex": "graph-1", "labelValue": ""},
-    {"label": "CSS", "value": 10, "colorIndex": "graph-1", "labelValue": ""},
-    {"label": "React", "value": 10, "colorIndex": "graph-3", "labelValue": ""},
-    {"label": "Vue", "value": 10, "colorIndex": "graph-3", "labelValue": ""},
 
-    {"label": "Scala", "value": 10, "colorIndex": "accent-2", "labelValue": ""},
-    {"label": "Java", "value": 10, "colorIndex": "accent-2", "labelValue": ""},
+    {"label": "Javascript", "value": 25, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "HTML", "value": 5, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "CSS", "value": 5, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "React", "value": 10, "colorIndex": "graph-1", "labelValue": ""},
+    {"label": "Vue", "value": 5, "colorIndex": "graph-1", "labelValue": ""},
+
+    {"label": "Scala", "value": 5, "colorIndex": "accent-1", "labelValue": ""},
     {"label": "Spark", "value": 15, "colorIndex": "graph-3", "labelValue": ""},
+    {"label": "Databricks", "value": 10, "colorIndex": "graph-3", "labelValue": ""},
     {"label": "Docker", "value": 10, "colorIndex": "graph-3", "labelValue": ""},
+    {"label": "Sagemaker", "value": 10, "colorIndex": "graph-3", "labelValue": ""},
+    {"label": "Stepfunctions", "value": 10, "colorIndex": "graph-3", "labelValue": ""},
     {"label": "Kubernetes", "value": 5, "colorIndex": "graph-3", "labelValue": ""},
     {"label": "MongoDB", "value": 5, "colorIndex": "graph-3", "labelValue": ""},
     {"label": "Redis", "value": 5, "colorIndex": "graph-3", "labelValue": ""}
